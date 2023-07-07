34dfedaa6356ca771248e77728a5ca1082dc63c8
izinhah <133233107+izinhah@users.noreply.github.com>
2023-07-07 11:59:56 +0000

    ISA

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ function carregar() {
     if (hora >= 0 && hora <= 12) {
         // Bom Dia! 
         img.src = 'pexels-ian-beckley-2440078.png'
+        document.body.style.background = '#f79367'
     } else if (hora >= 12 && hora <= 18) {
         // Boa Tarde! 
-        img.src = 'pexels-guillaume-hankenne-2792070.png'
+        img.src = 'pexels-guillaume-hankenne-2792070.png' 
+        document.body.style.background = '#d15f5f'
     } else {
         // Boa Noite!
-        img.src = 'pexels-allan-carvalho-17336461.png'
+        img.src = 'pexels-allan-carvalho-17336461.png' 
+        document.body.style.background = '#5d3257'
     }
 }
