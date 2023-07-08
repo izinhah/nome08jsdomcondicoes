fdfc6c3c57978b9a9d793bfac36f1563c4e0394e
izinhah <133233107+izinhah@users.noreply.github.com>
2023-07-08 01:29:36 +0000

    ISA

--- modelo02/script.js
+++ modelo02/script.js
@@ -8,15 +8,43 @@ function verificar() {
     } else { 
         var fsex = document.getElementsByName('radsex') 
         var idade = ano - Number(fano.value) 
-        var gênero = ''
+        var gênero = '' 
+        var img = document.createElement('img')
+        img.setAtributte('id','foto')
             if(fsex[0].checked){
                 gênero = 'Homem'
-
-            }else if (fsex[1].checked) {
-                gênero = 'Mulher'
+                if (idade >=0 && idade 10) {
+                    // Criança 
+                    img.setAtributte('src', 'criança-menino.png')
+                } else if (idade < 21) {
+                    //Jovem 
+                    img.setAtributte('src', 'jovem-garoto.png')
+                } else if (idade < 50) {
+                    //Adulto
+                    img.setAtributte('src', 'homem-adulto.png')
+                } else {
+                    //idoso 
+                    img.setAtributte('src', 'idoso.png')
+                }
+            } else if (fsex[1].checked) {
+                gênero = 'Mulher' 
+                if (idade >=0 && idade 10) {
+                    // Criança 
+                    img.setAtributte('src', 'criança-menina.png')
+                } else if (idade < 21) {
+                    //Jovem 
+                    img.setAtributte('src', 'jovem-garota.png')
+                } else if (idade < 50) {
+                    //Adulto 
+                    img.setAtributte('src', 'mulher-adulta.png')
+                } else {
+                    // Idoso 
+                    img.setAtributte('src', 'idosa.png')
+                }
             } 
             res.style.textAlign = 'center'
-            res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+            res.innerHTML = `Detectamos ${gênero} com ${idade} anos.` 
+            res.appendChild(img)
     }
 
 }
